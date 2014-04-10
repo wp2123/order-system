93d22b343e658f5cf5253f0cbb4adc766117ef3f
wp2123 <wangpu2123@sina.com>
2014-04-11 06:29:32 +0800

    it's the beginning of my graduation project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OrderSystem::Application.config.secret_key_base = 'fdd6f3193b9d95ccc4410849421d19b914ae80fd83caa59162a6d79e8b2881d563f8d22660f4bfd22c0456816aa9eeb70dd798e76a1528fa7b62861f9fd1b178'

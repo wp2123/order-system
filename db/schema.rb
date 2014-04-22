016a666cf8366b0d0ab219cdbebeaa0c090e6eba
wp2123 <wangpu2123@sina.com>
2014-04-22 15:56:25 +0800

    add campany_id to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140411081119) do
+ActiveRecord::Schema.define(version: 20140422075303) do
 
   create_table "users", force: true do |t|
     t.string   "username"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140411081119) do
     t.string   "role"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "company_id"
   end
 
 end
